e52732615fd80e671314ffeb9d35c62ad204f055
deepika-arora <deepikaaws05@gmail.com>
2025-12-12 22:13:04 +0530

    capitalize and upper function

--- string_function.py
+++ string_function.py
@@ -1,5 +1,5 @@
 # string concatintion
-'''first_name=input("enter first_name: ")
+first_name=input("enter first_name: ")
 last_name=input("enter last_name: ")
 age=input("enter your age: ")
 full_name=first_name+" "+last_name
@@ -8,7 +8,7 @@ print("full_name: ",full_name)
 sentence="hello this is my name: "+full_name+" and this is my age: "+age
 print(sentence)
  # formated string
-print(f'hello this is my name {full_name} and this is my age {age}')'''
+print(f'hello this is my name {full_name} and this is my age {age}')
 
 # .title():-> for first word capital
 first_name=input("enter first_name: ") .title()
@@ -17,3 +17,16 @@ age=input("enter your age: ")
 full_name=first_name+" "+last_name
 print("full_name: ",full_name)
 print(f'hello this is my name {full_name} and this is my age {age}')
+
+# capitalize():-> capitalize the first letter of the word of the sentence or paragraph
+sentence= "capitalize the first letter of the word of the sentence or paragraph".capitalize()
+print(sentence)
+
+# upper():-> convert all the letter into uppercase
+first_name=input("enter first_name: ").upper()
+last_name=input("enter last_name: ").upper()
+age=input("enter your age: ")
+full_name=first_name+" "+last_name
+print("full_name: ",full_name)
+sentence= "capitalize the first letter of the word of the sentence or paragraph".upper()
+print(sentence)
