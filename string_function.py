d4b7484a798a954bf8953f26a8936a97e24b1226
deepika-arora <deepikaaws05@gmail.com>
2025-12-12 21:46:22 +0530

    string function

---
+++ string_function.py
@@ -0,0 +1,9 @@
+# string concatintion
+first_name=input("enter first_name: ")
+last_name=input("enter last_name: ")
+age=input("enter your age: ")
+full_name=first_name+" "+last_name
+print("full_name: ",full_name)
+
+sentence="hello this is my name: "+full_name+" and this is my age: "+age
+print(sentence)
